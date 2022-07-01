4db8e2fa7940be54657896d9f9bc982ce18fab7f
DESKTOP-0HO1T46\Wahyu <wahyubudi860@gmail.com>
2022-07-01 14:36:12 +0700

    proses_pengajuan.php

--- proses_pengajuan.php
+++ proses_pengajuan.php
@@ -5,12 +5,12 @@ $data = json_decode(file_get_contents('php://input'), true);
 $jumlah = $data['jumlah'];
 $min = $data['min'];
 
-if ($jumlah < $min) {
+if ($jumlah <= $min) {
      
      $tanggal_pengajuan = $data['tanggal_pengajuan'];
      $id_nasabah = $data['id_nasabah'];
      
-     $sql = "INSERT INTO pengajuan (id_pengajuan, jumlah, tanggal_pengajuan, tanggal_penarikan, status, id_nasabah) VALUE ('NULL', '$jumlah', '$tanggal_pengajuan', '$tanggal_pengajuan', 'Sedang diproses','$id_nasabah' )";
+     $sql = "INSERT INTO pengajuan (id_pengajuan, jumlah, tanggal_pengajuan, status, id_nasabah) VALUE ('NULL', '$jumlah', '$tanggal_pengajuan', 'Sedang diproses','$id_nasabah' )";
      $query = mysqli_query($db, $sql);
 
      
